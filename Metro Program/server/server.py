4868dccaeddc6a1cecdfab2ad6c0a87ebfa454f4
Domar987 <omerserifoduncu@gmail.com>
2026-04-15 18:34:48 +0300

    bazı kod hataları giderildi

--- Metro Program/server/server.py
+++ Metro Program/server/server.py
@@ -1,12 +1,6 @@
 import json
 import socket
 
-userspath = "usersdata.json"
-fopen = open(userspath,"r")
-udatabase = json.load(fopen)
-
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(('127.0.0.1',8080))
 
 def getMsg():
     s.listen()
@@ -17,6 +11,7 @@ def getMsg():
         if not data:
             break
         else:
+            print("Mesaj alındı")
             readMsg(sock,data.decode())
         #sock.sendall(data)
     sock.close()
@@ -25,10 +20,24 @@ def readMsg(sock,msg:str):
     msgicerik = msg.split("-")
     if msgicerik[2][0]=='B':
         if msgicerik[2][1]=='Y':
-            miktar = eval(msgicerik[3])
+            for i in udatabase:
+                if i["isim"] == msgicerik[0] and i["sifre"] == msgicerik[1]:
+                    i["bakiye"] += eval(msgicerik[3])
+                    fwrite = open(userspath,"w")
+                    json.dump(udatabase,fwrite)
         elif msgicerik[2][1]=='S':
             for i in udatabase:
                 if i["isim"] == msgicerik[0] and i["sifre"] == msgicerik[1]:
-                    sock.sendall("test")
+                    sock.sendall(str(i["bakiye"]).encode())
     elif msgicerik[3][0]=='A':
         pass
+
+userspath = "usersdata.json"
+fread = open(userspath,"r")
+udatabase = json.load(fread)
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind(('127.0.0.1',8080))
+
+while True:
+    getMsg()
--- Metro Program/yolcu.py
+++ Metro Program/yolcu.py
@@ -3,7 +3,7 @@ import os
 
 class Yolcu():
     def __init__(self,isim:str,sifre:str):
-        self.ip=os.environ["REMOTE_ADDR"]
+        self.ip='localhost'#os.environ["REMOTE_ADDR"]
         self.isim=isim
         self.sifre=sifre
         self.port=8080
@@ -11,3 +11,7 @@ class Yolcu():
         client.connectToServer(f"{self.isim}-{self.sifre}-BS")
     def bakiyeYukle(self,miktar):
         client.connectToServer(f"{self.isim}-{self.sifre}-BY-{miktar}")
+
+yolcu = Yolcu("aasd","1234")
+yolcu.bakiyeSorgu()
+yolcu.bakiyeYukle(100)
